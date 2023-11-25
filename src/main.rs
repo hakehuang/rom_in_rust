0f053e144ef9f597781545616571608a1a98c9c2
Hake Huang <hake.huang@oss.nxp.com>
2023-11-25 22:51:32 +0800

    update code to based on cortex-m-rt
    Signed-off-by: Hake Huang <hake.huang@oss.nxp.com>

--- src/main.rs
+++ src/main.rs
@@ -3,70 +3,53 @@
 #![allow(dead_code)]
 #![allow(unused_variables)]
 
-use core::panic::PanicInfo;
-use core::ptr;
+extern crate cortex_m_rt as rt;
+extern crate panic_halt;
 
-// The reset handler
+use rt::entry;
+
+const FFRO_STABLE_TIME: u32 = 12;
+const SFRO_STABLE_TIME: u32 = 13;
+const FIRC_48MHZ_TRIM_TEMPCO: u32 = 48;
+const FIRC_48MHZ_TRIM_COARSE: u32 = 49;
+const FIRC_48MHZ_TRIM_FINE: u32 = 50;
+const FIRC_60MHZ_TRIM_TEMPCO: u32 = 51;
+const FIRC_60MHZ_TRIM_COARSE: u32 = 52;
+const FIRC_60MHZ_TRIM_FINE: u32 = 53;
+
 #[no_mangle]
-pub unsafe extern "C" fn Reset() -> ! {
-    extern "C" {
-        static mut _sbss: u8;
-        static mut _ebss: u8;
+#[link_section = ".ROM_API_0.FSL_ROM_OTP_INIT_ADDR"]
+pub unsafe extern "C" fn FSL_ROM_OTP_INIT_ADDR(src_clk_freq: u32) -> i32 {
+    return 0;
+}
 
-        static mut _sdata: u8;
-        static mut _edata: u8;
-        static _sidata: u8;
+#[no_mangle]
+#[link_section = ".ROM_API_1.FSL_ROM_OTP_DEINIT_ADDR"]
+pub unsafe extern "C" fn FSL_ROM_OTP_DEINIT_ADDR() -> i32 {
+    return 0;
+}
+#[no_mangle]
+#[link_section = ".ROM_API_2.FSL_ROM_OTP_FUSE_READ_ADDR"]
+pub unsafe extern "C" fn FSL_ROM_OTP_FUSE_READ_ADDR(addr: u32, data: &mut u32) -> i32 {
+    /*all fake data here*/
+    match addr {
+        FFRO_STABLE_TIME => *data = 0x10,
+        SFRO_STABLE_TIME => *data = 0x10,
+        FIRC_48MHZ_TRIM_TEMPCO => *data = 0x10,
+        FIRC_48MHZ_TRIM_COARSE => *data = 0x20,
+        FIRC_48MHZ_TRIM_FINE => *data = 0x40,
+        FIRC_60MHZ_TRIM_TEMPCO => *data = 0x10,
+        FIRC_60MHZ_TRIM_COARSE => *data = 0x20,
+        FIRC_60MHZ_TRIM_FINE => *data = 0x40,
+        _ => *data = 0,
     }
 
-    let count = &_ebss as *const u8 as usize - &_sbss as *const u8 as usize;
-    ptr::write_bytes(&mut _sbss as *mut u8, 0, count);
-
-    let count = &_edata as *const u8 as usize - &_sdata as *const u8 as usize;
-    ptr::copy_nonoverlapping(&_sidata as *const u8, &mut _sdata as *mut u8, count);
-
-    main();
-}
-
-#[no_mangle]
-pub unsafe extern "C" fn FSL_ROM_OTP_INIT_ADDR_CALL(src_clk_freq: u32) -> i32 {
     return 0;
 }
 
-#[no_mangle]
-pub unsafe extern "C" fn FSL_ROM_OTP_DEINIT_ADDR_CALL() -> i32 {
-    return 0;
-}
-#[no_mangle]
-pub unsafe extern "C" fn FSL_ROM_OTP_FUSE_READ_ADDR_CALL(addr: u32, data: *const u32) -> i32 {
-    return 0;
-}
-
+#[entry]
 fn main() -> ! {
     loop {
         // your code goes here
     }
-}
-
-// The reset vector, a pointer into the reset handler
-#[link_section = ".vector_table.reset_vector"]
-#[no_mangle]
-pub static RESET_VECTOR: unsafe extern "C" fn() -> ! = Reset;
-
-#[link_section = ".OPT_API_CALL.FSL_ROM_OTP_INIT_ADDR"]
-#[no_mangle]
-pub static FSL_ROM_OTP_INIT_ADDR: unsafe extern "C" fn(src_clk_freq: u32) -> i32 =
-    FSL_ROM_OTP_INIT_ADDR_CALL;
-
-#[link_section = ".OPT_API_CALL.FSL_ROM_OTP_DEINIT_ADDR"]
-#[no_mangle]
-pub static FSL_ROM_OTP_DEINIT_ADDR: unsafe extern "C" fn() -> i32 = FSL_ROM_OTP_DEINIT_ADDR_CALL;
-
-#[link_section = ".OPT_API_CALL.FSL_ROM_OTP_FUSE_READ_ADDR"]
-#[no_mangle]
-pub static FSL_ROM_OTP_FUSE_READ_ADDR: unsafe extern "C" fn(addr: u32, data: *const u32) -> i32 =
-    FSL_ROM_OTP_FUSE_READ_ADDR_CALL;
-
-#[panic_handler]
-fn panic(_panic: &PanicInfo<'_>) -> ! {
-    loop {}
 }
